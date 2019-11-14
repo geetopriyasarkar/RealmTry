a69d0738bc0d0fc300d4f931f2c33f68651fb4bb
Manmohan <manmohan@macmini.local>
2019-11-14 13:09:21 +0530

    Initial Commit

---
+++ RealmTry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmTry
+//
+//  Created by Manmohan on 14/11/19.
+//  Copyright © 2019 Geet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
